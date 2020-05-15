9ae083b55d5d5c22a91df7422aed24f2d3885e54
Felipe Carvalho Funck <funck.felipe@hotmail.com>
2020-05-14 21:09:13 -0300

    Create synchronous event producer

--- library-event/src/main/java/com/learnkafka/producer/LibraryEventProducer.java
+++ library-event/src/main/java/com/learnkafka/producer/LibraryEventProducer.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.learnkafka.domain.LibraryEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -12,10 +15,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.List;
+
 @Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Component
 public class LibraryEventProducer {
+
+    private static final String TOPIC = "library-events";
 
     private final KafkaTemplate<Integer, String> template;
     private final ObjectMapper mapper;
@@ -26,6 +33,31 @@ public class LibraryEventProducer {
         String value = mapper.writeValueAsString(event.getBook());
         ListenableFuture<SendResult<Integer, String>> listenableFuture = template.sendDefault(key, value);
         listenableFuture.addCallback(callback);
+    }
+
+    public void sendLibraryEventWithTopic(final LibraryEvent event) throws JsonProcessingException {
+        Integer key = event.getId();
+        String value = mapper.writeValueAsString(event.getBook());
+        ListenableFuture<SendResult<Integer, String>> listenableFuture = template.send(buildProducerRecord(key, value, TOPIC));
+        listenableFuture.addCallback(callback);
+    }
+
+    public SendResult<Integer, String> sendLibraryEventSynchronous(final LibraryEvent event) throws Exception {
+        Integer key = event.getId();
+        String value = mapper.writeValueAsString(event.getBook());
+        SendResult<Integer, String> sendResul = null;
+        try {
+             sendResul = template.sendDefault(key, value).get();
+        } catch (Exception e) {
+            log.error("Exception sending the message and the exception is {}", e.getMessage());
+            throw e;
+        }
+        return sendResul;
+    }
+
+    private ProducerRecord<Integer, String> buildProducerRecord(Integer key, String value, String topic) {
+        final List<Header> headers = List.of(new RecordHeader("event-source", "scanner".getBytes()));
+        return new ProducerRecord(TOPIC, null, key, value, headers);
     }
 
 }
--- library-event/src/main/java/com/learnkafka/resource/LibraryEventResource.java
+++ library-event/src/main/java/com/learnkafka/resource/LibraryEventResource.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
 import com.learnkafka.producer.LibraryEventProducer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
+@Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @RestController
 @RequestMapping("/v1/library-events")
@@ -21,10 +24,20 @@ public class LibraryEventResource {
 
     private final LibraryEventProducer producer;
 
-    @PostMapping
+    @PostMapping(path = "asynchronous")
     @ResponseStatus(CREATED)
-    public ResponseEntity<LibraryEvent> save(@RequestBody final LibraryEvent libraryEvent) throws JsonProcessingException {
+    public ResponseEntity<LibraryEvent> saveAsynchronous(@RequestBody final LibraryEvent libraryEvent) throws JsonProcessingException {
+        log.info("Sending asynchronous message");
         producer.sendLibraryEvent(libraryEvent);
+        return ResponseEntity.ok(libraryEvent);
+    }
+
+    @PostMapping(path = "synchronous")
+    @ResponseStatus(CREATED)
+    public ResponseEntity<LibraryEvent> saveSynchronous(@RequestBody final LibraryEvent libraryEvent) throws Exception {
+        log.info("Sending synchronous message");
+        SendResult<Integer, String> sendResult = producer.sendLibraryEventSynchronous(libraryEvent);
+        log.info("SendResult is {}", sendResult);
         return ResponseEntity.ok(libraryEvent);
     }
 
