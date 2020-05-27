76833cc2aa8ca76fc1f6eb2d9c4b81377dbad6ed
Felipe Carvalho Funck <funck.felipe@hotmail.com>
2020-05-27 20:17:13 -0300

    Config kafka for save message in all particions

--- library-event/src/main/java/com/learnkafka/resource/LibraryEventResource.java
+++ library-event/src/main/java/com/learnkafka/resource/LibraryEventResource.java
@@ -9,7 +9,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.support.SendResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
